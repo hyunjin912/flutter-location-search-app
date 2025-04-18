62b4c9b25af1c2a7dedcabaa2c25d318858d7bea
leehyunjin <skyhyunjinlee@gmail.com>
2025-04-18 12:15:56 +0900

    refactor: http라이브러리 삭제, dio라이브러리 추가
    dio라이브러리에 맞게 location_repository 수정

--- lib/data/repository/location_repository.dart
+++ lib/data/repository/location_repository.dart
@@ -1,31 +1,28 @@
-import 'dart:convert';
+import 'package:dio/dio.dart';
 import 'package:flutter_loaction_search_app/data/model/location_model.dart';
-import 'package:http/http.dart';
 
 class LocationRepository {
   Future<List<Location>> search(String query) async {
     try {
-      final client = Client();
-      final result = await client.get(
-        Uri.parse(
-          'https://openapi.naver.com/v1/search/local.json?query=$query&display=5',
+      final Dio _client = Dio(
+        BaseOptions(
+          // 설정 안 하면, 실패 응답 올 때 throw 던져서 에러 발생
+          validateStatus: (status) => true,
+          headers: {
+            'X-Naver-Client-Id': 'NMPhvQCPL0eTHF0tINgB',
+            'X-Naver-Client-Secret': 'HtBLoalFc6',
+          },
         ),
-        headers: {
-          'X-Naver-Client-Id': 'NMPhvQCPL0eTHF0tINgB',
-          'X-Naver-Client-Secret': 'HtBLoalFc6',
-        },
       );
 
-      if (result.statusCode == 200) {
-        final map = jsonDecode(result.body);
+      final response = await _client.get(
+        'https://openapi.naver.com/v1/search/local.json',
+        queryParameters: {'query': query, 'display': 5},
+      );
 
-        // 여기서 List.from을 하지 않고
-        // items = map('items'); 를 하면 items의 타입은 dynamic이 된다
-        // List일게 분명한데도 말이다.
-        // 그래서 List.from으로 복사를 하면 타입이 List<dynamic>이 된다
-        // List<dynamic>이 돼야 final list의 타입이 List<Locaation>으로 제대로 된다
-        final items = List.from(map['items']);
-
+      if (response.statusCode == 200) {
+        final items = List.from(response.data['items']);
+        print(items);
         final iterable = items.map((e) => Location.fromJson(e));
         final list = iterable.toList();
 
